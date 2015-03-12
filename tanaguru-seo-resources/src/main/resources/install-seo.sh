6580780486afe5e810bb96d5c2d2175aa3c38f15
Jérôme Kowalczyk <jk@tanaguru.com>
2015-03-12 18:26:09 +0100

    bump from version 1.0.2 to 1.0.3

--- tanaguru-seo-resources/src/main/resources/install-seo.sh
+++ tanaguru-seo-resources/src/main/resources/install-seo.sh
@@ -15,8 +15,9 @@ sed -i -e "s#</beans>#\t<import resource=\"classpath:conf/context/seo/engine/seo
 	    "$TOMCAT_HOST_PATH/WEB-INF/conf/tgol-service.xml"
 
 # Add i18n files 
-sed -i -e "s#<!--rules-commons i18n#<!--seo2.2 i18n files -->\n\t\t<value>i18n/referential-seo-I18N</value>\n\t\t<value>i18n/theme-seo-I18N</value>\n\t\t<value>i18n/criterion-seo-I18N</value>\n\t\t<value>i18n/rule-seo-I18N</value>\n\n\t\t<!--rules-commons i18n#" \
+sed -i -e "s#<!--rules-commons i18n#<!--seo1.0 i18n files -->\n\t\t<value>classpath:i18n/referential-seo-I18N</value>\n\t\t<value>classpath:i18n/theme-seo-I18N</value>\n\t\t<value>classpath:i18n/criterion-seo-I18N</value>\n\t\t<value>classpath:i18n/rule-seo-I18N</value>\n\n\t\t<!--rules-commons i18n#" \
 	    "$TOMCAT_HOST_PATH/WEB-INF/conf/mvc/tgol-beans-i18n.xml"
 
 # Add specific test representations
-echo -e "\r# Seo 1.0 test representation overwrite\rSeo-01021-representation-index=3\rSeo-01041-representation-index=4\rSeo-06041-representation-index=3\rSeo-07061-representation-index=3\r" >> "$TOMCAT_HOST_PATH/WEB-INF/classes/representation.properties"
+echo -e "\n# Seo 1.0 test representation overwrite\nSeo-01021-representation-index=3\nSeo-01041-representation-index=4\nSeo-06041-representation-index=3\nSeo-07061-representation-index=3\n:q!
+" >> "$TOMCAT_HOST_PATH/WEB-INF/classes/representation.properties"
